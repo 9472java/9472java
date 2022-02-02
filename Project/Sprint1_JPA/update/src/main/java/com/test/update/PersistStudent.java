8361599743af5f425f2961f8de8299e8f3cb22cd
Pandurang Kapate <95092513+pjkapate1996@users.noreply.github.com>
2022-02-02 11:33:11 +0530

    Add files via upload

---
+++ Project/Sprint1_JPA/update/src/main/java/com/test/update/PersistStudent.java
@@ -0,0 +1,42 @@
+package com.test.update;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.test.update.entity.StudentEntity;
+
+
+public class PersistStudent 
+{
+    public static void main( String[] args )
+    {
+        EntityManagerFactory emf=Persistence.createEntityManagerFactory("Student_update");  
+        EntityManager em=emf.createEntityManager(); 
+        
+        
+        em.getTransaction().begin();        
+	      	          
+       StudentEntity s=em.find(StudentEntity.class,101);  
+	        System.out.println("Before Updation");  
+            System.out.println("Student id = "+s.getS_id());  
+            System.out.println("Student Name = "+s.getS_name());  
+	        System.out.println("Student Age = "+s.getS_age());  
+	          
+	s.setS_name("pandurang");  
+	          
+	        System.out.println("After Updation"); 
+	        System.out.println("Student id = "+s.getS_id());  
+	        System.out.println("Student Name = "+s.getS_name());  
+	        System.out.println("Student Age = "+s.getS_age());
+	        
+	              
+	          
+	        em.getTransaction().commit();  
+	          
+	        emf.close();  
+	        em.close();    
+	    }  
+
+    }
+
+
